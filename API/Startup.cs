cb18b1cf865678952073a6dfd3752ab7eeb47717
BBump <brbumpas@hotmail.com>
2021-11-23 03:42:03 -0600

    section 3 changes

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,7 @@ namespace API
             services.AddDbContext<DataContext>(options => {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +53,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
